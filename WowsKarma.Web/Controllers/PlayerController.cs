b4f1fa37ded149264ee1c3944e9a21c015b3c71e
DJ Daemonix <dj.daemonix@nodsoft.net>
2020-12-21 16:53:35 +0100

    Draft PlayerController

---
+++ WowsKarma.Web/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WowsKarma.Web.Controllers
+{
+	[Route("player")]
+	public class PlayerController : ControllerBase
+	{
+		public IActionResult Index()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Route("{id},{name}")]
+		public IActionResult Profile(uint id)
+		{
+			return StatusCode(200, $"WOWS Karma \n{id}");
+		}
+	}
+}

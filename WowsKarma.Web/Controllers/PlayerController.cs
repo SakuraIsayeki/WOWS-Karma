cfaf550f0fd64557c7f4dde9fe9f54a6a48ac5f9
DJ Daemonix <dj.daemonix@nodsoft.net>
2020-12-22 06:14:08 +0100

    Strip out all gRPC Components
    Preparing for a switch to REST APIs

--- WowsKarma.Web/Controllers/PlayerController.cs
+++ WowsKarma.Web/Controllers/PlayerController.cs
@@ -1,23 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using WowsKarma.Common;
-using WowsKarma.Web.Models;
-using WowsKarma.Web.Models.ViewModels;
+
+
 
 namespace WowsKarma.Web.Controllers
 {
 	public class PlayerController : Controller
 	{
-		private readonly ApiService.ApiServiceClient _apiServiceClient;
-
-		public PlayerController(ApiService.ApiServiceClient client)
-		{
-			_apiServiceClient = client;
-		}
+		public PlayerController() { }
 
 		public IActionResult Index() => View();
 
@@ -25,21 +16,12 @@ namespace WowsKarma.Web.Controllers
 		[Route("/{controller}/{id},{name}")]
 		public async Task<IActionResult> Profile(uint id)
 		{
-			GetAccountResponse response = await _apiServiceClient.GetAccountAsync(new() { AccountId = id });
-			return View(response.Account);
+			throw new NotImplementedException();
 		}
 
 		public async Task<IActionResult> Search(string id)
 		{
-			ListAccountsResponse result = await _apiServiceClient.ListAccountsAsync(new() { Search = id });
-			List<PlayerListing> listings = new();
-
-			foreach (ListAccountsResponse.Types.Result listing in result.Results)
-			{
-				listings.Add(new() { Id = listing.AccountId, Username = listing.Username });
-			}
-
-			return View(new SearchViewModel<PlayerListing>() { Search = id, Results = listings });
+			throw new NotImplementedException();
 		}
 	}
 }
--- WowsKarma.Web/Startup.cs
+++ WowsKarma.Web/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WowsKarma.Common;
+
+
 
 namespace WowsKarma.Web
 {
@@ -25,7 +21,6 @@ namespace WowsKarma.Web
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
-			services.AddGrpcClient<ApiService.ApiServiceClient>(o => o.Address = new Uri(Configuration["Api:Host"]));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

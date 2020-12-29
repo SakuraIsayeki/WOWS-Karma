05cfdffe7087b3c09130fe88283d688e2457c276
DJ Daemonix <dj.daemonix@nodsoft.net>
2020-12-29 16:29:05 +0100

    Trim unused assets

--- WowsKarma.Web/Program.cs
+++ WowsKarma.Web/Program.cs
@@ -1,13 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WowsKarma.Web

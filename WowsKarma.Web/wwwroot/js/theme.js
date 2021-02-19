b739369043afd85a50503c67b3526588e9445990
Sakura Isayeki <sakura.isayeki@nodsoft.net>
2021-02-19 04:26:41 +0100

    Rework Web Themes implementation
    As this is a client-only matter, it feels unnecessary to force more load onto Blazor by making calls back to web app.
    
    So the new implementation is JavaScript-based. Quick, simple, and dirty, but ultimately cheaper on server-load.

---
+++ WowsKarma.Web/wwwroot/js/theme.js
@@ -0,0 +1,30 @@
+﻿window.addEventListener('DOMContentLoaded', setThemeDisplay);
+
+function setThemeDisplay() {
+	if (localStorage['theme'] === 'light') {
+		document.querySelector("#light-theme").removeAttribute('disabled');
+		document.querySelector("#dark-theme").setAttribute('disabled', true);
+	}
+	else {
+		document.querySelector("#light-theme").setAttribute('disabled', true);
+		document.querySelector("#dark-theme").removeAttribute('disabled');
+	}
+}
+
+function setTheme() {
+	//Invert theme
+	var theme = localStorage['theme'] === 'light' ? 'dark' : 'light'; 
+
+	localStorage['theme'] = theme;
+	document.querySelector("#icon-theme-selector").setAttribute('class', getThemeSelectorIcon());
+	setThemeDisplay();
+}
+
+function getThemeSelectorIcon() {
+	if (localStorage['theme'] === 'light') {
+		return 'bi bi-moon';
+	}
+	else {
+		return 'bi bi-sun';
+	}
+}

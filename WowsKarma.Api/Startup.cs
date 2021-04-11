2c3c28627daaeea20ab0da388cc620755019951c
Sakura Isayeki <sakura.isayeki@nodsoft.net>
2021-04-11 12:13:34 +0200

    Update Packages + Docker Hostnames

--- WowsKarma.Api/Startup.cs
+++ WowsKarma.Api/Startup.cs
@@ -1,14 +1,10 @@
-using AspNetCore.Authentication.ApiKey;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using Wargaming.WebAPI;
 using Wargaming.WebAPI.Models;
 using Wargaming.WebAPI.Requests;

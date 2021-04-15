d44bcd6755ab91981e8c194b055ae58ccccc5c95
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-04-15 14:17:21 +0200

    Implement HtmlContentFileLoader + switched Index Page to dynamic content

---
+++ WowsKarma.Web/Services/HtmlContentFileLoader.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.IO;
+
+
+
+namespace WowsKarma.Web.Services
+{
+	public static class HtmlContentFileLoader
+	{
+		public const string FileExtension = ".html";
+
+		internal static MarkupString LoadHtmlContent(FileInfo fileInfo) => new(File.ReadAllText(fileInfo.FullName));
+		public static MarkupString LoadHtmlContent(string fileName)
+		{
+			fileName = fileName.EndsWith(FileExtension) ? fileName : (fileName + FileExtension);
+			FileInfo file = new(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets", fileName));
+			return new(File.ReadAllText(file.FullName));
+		}
+	}
+}

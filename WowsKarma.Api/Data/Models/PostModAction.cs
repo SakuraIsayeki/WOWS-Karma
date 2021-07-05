81e2ec9fe5d770765d01c10e12c181fd891592ed
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-07-05 06:45:25 +0200

    Add PostModAction Data Model + Migrations

---
+++ WowsKarma.Api/Data/Models/PostModAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WowsKarma.Common.Models;
+
+namespace WowsKarma.Api.Data.Models
+{
+	public record PostModAction
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public Guid Id { get; init; }
+
+		public Post Post { get; init; }
+		public Guid PostId { get; init; }
+
+
+
+		public ModActionType ActionType { get; init; }
+
+		public Player Mod { get; init; }
+		public uint ModId { get; init; }
+
+		public string Reason { get; set; }
+	}
+}
---
+++ WowsKarma.Api/Migrations/ApiDb/20210705044134_AddPostModActions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WowsKarma.Common.Models;
+
+namespace WowsKarma.Api.Migrations.ApiDb
+{
+    public partial class AddPostModActions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:mod_action_type", "deletion,update");
+
+            migrationBuilder.CreateTable(
+                name: "PostModActions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PostId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActionType = table.Column<ModActionType>(type: "mod_action_type", nullable: false),
+                    ModId = table.Column<long>(type: "bigint", nullable: false),
+                    Reason = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostModActions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostModActions_Players_ModId",
+                        column: x => x.ModId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostModActions_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostModActions_ModId",
+                table: "PostModActions",
+                column: "ModId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostModActions_PostId",
+                table: "PostModActions",
+                column: "PostId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostModActions");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:Enum:mod_action_type", "deletion,update");
+        }
+    }
+}

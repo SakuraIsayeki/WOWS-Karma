ebcd6de98de31e3001b589327c5f1f27361910af
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-07-30 01:16:41 +0200

    Include IP readout in Console Logging

--- WowsKarma.Api/Middlewares/RequestLoggingMiddleware.cs
+++ WowsKarma.Api/Middlewares/RequestLoggingMiddleware.cs
@@ -12,10 +12,10 @@ namespace WowsKarma.Api.Middlewares
 {
 	public class RequestLoggingMiddleware
 	{
-		private const string MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+		private const string MessageTemplate = "HTTP {RequestMethod} {RequestPath} by {RemoteHost}, responded {StatusCode} in {Elapsed:0.00} ms";
 
 		private static readonly ILogger logger = Log.ForContext<RequestLoggingMiddleware>();
-		private static readonly HashSet<string> HeaderWhitelist = new() {"Content-Type", "Content-Length", "User-Agent"};
+		private static readonly HashSet<string> HeaderWhitelist = new() { "Content-Type", "Content-Length", "User-Agent" };
 
 		private readonly RequestDelegate next;
 
@@ -40,7 +40,7 @@ namespace WowsKarma.Api.Middlewares
 
 				ILogger log = level is LogEventLevel.Error ? LogForErrorContext(context) : logger.ForContext("RequestUser", context.Connection?.RemoteIpAddress?.ToString());
 
-				log.Write(level, MessageTemplate, context.Request.Method, GetPath(context), statusCode, elapsedMs);
+				log.Write(level, MessageTemplate, context.Request.Method, GetPath(context), GetRemoteHost(context), statusCode, elapsedMs);
 			}
 
 			// Never caught, because `LogException()` returns false.
@@ -49,7 +49,7 @@ namespace WowsKarma.Api.Middlewares
 
 		private static bool LogException(HttpContext httpContext, double elapsedMs, Exception ex)
 		{
-			LogForErrorContext(httpContext).Error(ex, MessageTemplate, httpContext.Request.Method, GetPath(httpContext), 500, elapsedMs);
+			LogForErrorContext(httpContext).Error(ex, MessageTemplate, httpContext.Request.Method, GetPath(httpContext), GetRemoteHost(httpContext), 500, elapsedMs);
 
 			return false;
 		}
@@ -73,5 +73,7 @@ namespace WowsKarma.Api.Middlewares
 		private static double GetElapsedMilliseconds(long start, long stop) => (stop - start) * 1000 / (double)Stopwatch.Frequency;
 
 		private static string GetPath(HttpContext context) => context.Features.Get<IHttpRequestFeature>()?.RawTarget ?? context.Request.Path.ToString();
+
+		private static string GetRemoteHost(HttpContext context) => context.Connection?.RemoteIpAddress?.ToString() ?? "Unknown";
 	}
 }

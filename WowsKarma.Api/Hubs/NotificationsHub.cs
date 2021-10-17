5db776d1f54f2432c704d8d125109a4a9839cb5d
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-10-17 16:40:56 +0200

    Draft Notifications Hub

---
+++ WowsKarma.Api/Hubs/NotificationsHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using WowsKarma.Common.Hubs;
+
+namespace WowsKarma.Api.Hubs;
+
+public class NotificationsHub : Hub<INotificationsHubPush>, INotificationsHubInvoke
+{
+	public Task AcknowledgeNotifications(Guid[] notificationIds) => throw new NotImplementedException();
+
+	public Task<IAsyncEnumerable<INotification>> GetPendingNotifications([EnumeratorCancellation] CancellationToken ct) => throw new NotImplementedException();
+}
---
+++ WowsKarma.Common/Hubs/INotificationsHub.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WowsKarma.Common.Models;
+
+
+
+namespace WowsKarma.Common.Hubs;
+
+
+public interface INotificationsHubPush
+{
+	public Task NewNotification(INotification notification);
+}
+
+public interface INotificationsHubInvoke
+{
+	public Task<IAsyncEnumerable<INotification>> GetPendingNotifications(CancellationToken ct);
+	public Task AcknowledgeNotifications(Guid[] notificationIds);
+}

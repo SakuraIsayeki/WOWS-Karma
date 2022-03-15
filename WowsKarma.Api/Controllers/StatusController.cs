fead4fbaba6110eca46d9081aa6827210682c4cc
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-03-15 20:13:22 +0100

    Implement UI restrictions for replay-less posts creation
    With this policy enabled, posts must fill either of 3 condition for the submit button to be clickable in the editor:
     - Post's Replay is not null
     - All post flairs are set to neutral
     - Post is being edited

--- WowsKarma.Api/Controllers/StatusController.cs
+++ WowsKarma.Api/Controllers/StatusController.cs
@@ -3,31 +3,31 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 
-namespace WowsKarma.Api.Controllers
+namespace WowsKarma.Api.Controllers;
+
+/// <summary>
+/// Provides status endpoints for controlling API lifetime.
+/// </summary>
+[ApiController, Route("api/[controller]")]
+public class StatusController : Controller
 {
 	/// <summary>
-	/// Provides status endpoints for controlling API lifetime.
+	/// Provides a HTTP ping endpoint.
 	/// </summary>
-	[ApiController, Route("api/[controller]")]
-	public class StatusController : Controller
+	/// <response code="200">Service is healthy.</response>
+	[HttpGet, ProducesResponseType(200)]
+	public IActionResult Status() => Ok();
+
+	[Route("/error"), ApiExplorerSettings(IgnoreApi = true)]
+	public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
 	{
-		/// <summary>
-		/// Provides a HTTP ping endpoint.
-		/// </summary>
-		/// <response code="200">Service is healthy.</response>
-		[HttpGet, ProducesResponseType(200)]
-		public IActionResult Status() => Ok();
-		
-		[Route("/error"), ApiExplorerSettings(IgnoreApi = true)]
-		public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
+		if (!hostEnvironment.IsDevelopment())
 		{
-			if (!hostEnvironment.IsDevelopment())
-			{
-				return NotFound();
-			}
-
-			IExceptionHandlerFeature exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
-
-			return Problem(detail: exceptionHandlerFeature?.Error.StackTrace, title: exceptionHandlerFeature?.Error.Message);
+			return NotFound();
 		}
+
+		IExceptionHandlerFeature exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+
+		return Problem(detail: exceptionHandlerFeature?.Error.StackTrace, title: exceptionHandlerFeature?.Error.Message);
+	}
 }

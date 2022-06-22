16b67f4422f25c2de9ddbbebdedd96a405a316be
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-06-22 16:58:20 +0200

    Add Region-specific PSA support

--- wowskarma.app/src/app/shared/layout/layout.component.ts
+++ wowskarma.app/src/app/shared/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConfigService } from "../../services/app-config.service";
 
 @Component({
   selector: 'app-layout',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+  currentRegion: string = this.appConfigService.currentRegion;
 
-  constructor() { }
+  constructor(private appConfigService: AppConfigService) { }
 
   ngOnInit(): void {
   }

4cc351c953bc699fa3013a24bff16d8b0dbf19fd
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-06-26 11:16:54 +0200

    Implement Post ModAction display on app

--- wowskarma.app/src/app/pages/post/view/view-post.component.ts
+++ wowskarma.app/src/app/pages/post/view/view-post.component.ts
@@ -1,19 +1,31 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { filter, map } from "rxjs/operators";
+import { ModActionService } from "../../../services/api/services/mod-action.service";
 import { PostService } from "../../../services/api/services/post.service";
-import { routeParam, shareReplayRefCount, switchMapCatchError } from "../../../shared/rxjs-operators";
+import { filterNotNull, routeParam, shareReplayRefCount, switchMapCatchError } from "../../../shared/rxjs-operators";
 
 @Component({
-  templateUrl: './view-post.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: "./view-post.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewPostComponent {
 // Get the "ID,username" from the route params.
-  post$ = routeParam(this.route)
-    .pipe(switchMapCatchError((id) =>
-        this.postService.apiPostPostIdGet$Json({ postId: id! })),
-      shareReplayRefCount(1));
+    post$ = routeParam(this.route)
+        .pipe(switchMapCatchError((id) =>
+                this.postService.apiPostPostIdGet$Json({ postId: id! })),
+            shareReplayRefCount(1));
 
-  constructor(private route: ActivatedRoute, private postService: PostService) {
-  }
+    lastModAction$ = this.post$.pipe(
+        filterNotNull(),
+        filter((post) => post.modLocked === true),
+        switchMapCatchError((post) => this.modActionService.apiModActionListGet$Json({ postId: post.id! }).pipe(
+            filterNotNull(),
+        )),
+        map((modActions) => modActions![0]),
+        shareReplayRefCount(1),
+    );
+
+    constructor(private route: ActivatedRoute, private postService: PostService, private modActionService: ModActionService) {
+    }
 }

99746d11a043d41391c65e273a1654e2cf98c003
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-07-15 17:00:07 +0200

    Add support for PWA

--- wowskarma.app/src/app/app.module.ts
+++ wowskarma.app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { ServiceWorkerModule } from "@angular/service-worker";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppWrapperComponent } from "./app-wrapper.component";
 import { AppComponent } from "./app.component";
@@ -14,6 +15,7 @@ import { ProfileComponent as PlayerProfileComponent } from "./pages/player/profi
 import { SearchComponent as PlayerSearchComponent } from "./pages/player/search/search.component";
 import { PostListComponent } from "./pages/post/list/post-list.component";
 import { ViewPostComponent } from "./pages/post/view/view-post.component";
+import { SettingsComponent } from "./pages/settings/settings.component";
 import { ApiModule } from "./services/api/api.module";
 import { AppConfigService } from "./services/app-config.service";
 import { AppInitService } from "./services/app-init.service";
@@ -23,36 +25,35 @@ import { AppInitGuard } from "./services/guards/app-init.guard";
 import { AuthGuard } from "./services/guards/auth.guard";
 import { AuthenticationInterceptor } from "./services/interceptors/authentication-interceptor.service";
 import { ErrorInterceptor } from "./services/interceptors/error.interceptor";
+import { BypassHtmlPipe } from "./services/pipes/bypass-html.pipe";
 import { ColorHexPipe } from "./services/pipes/colorHex.pipe";
 import { FormErrorsPipe } from "./services/pipes/form-errors.pipe";
+import { KarmaColorPipe } from "./services/pipes/karma-color.pipe";
+import { PostBorderColorPipe } from "./services/pipes/post-border-color.pipe";
 import { SafeStylePipe } from "./services/pipes/safe-style.pipe";
+import { WowsNumbersClanLinkPipe } from "./services/pipes/wows-numbers-clan-link.pipe";
+import { WowsNumbersPlayerLinkPipe } from "./services/pipes/wows-numbers-player-link.pipe";
+import { HtmlLoaderComponent } from "./shared/components/html-loader/html-loader.component";
+import { MinMaxMetricComponent } from "./shared/components/minmax-metric/min-max-metric.component";
 import { ControlExtensionsDirective } from "./shared/directives/control-extensions.directive";
 import { FormErrorsComponent } from "./shared/form-errors/form-errors.component";
 import { FooterComponent } from "./shared/layout/footer/footer.component";
 import { LayoutComponent } from "./shared/layout/layout.component";
 import { NavAuthComponent } from "./shared/layout/navbar/nav-auth/nav-auth.component";
 import { NavbarComponent } from "./shared/layout/navbar/navbar.component";
+import { NotificationsButtonComponent } from "./shared/layout/navbar/notifications-button/notifications-button.component";
+import { PostDeleteComponent } from "./shared/modals/post-delete/post-delete.component";
 import { PostEditorComponent } from "./shared/modals/post-editor/post-editor.component";
+import { PostModDeleteComponent } from "./shared/modals/post-mod-delete/post-mod-delete.component";
+import { SeedTokenChangeComponent } from "./shared/modals/seed-token-change/seed-token-change.component";
+import { NotificationComponent } from "./shared/notifications/notification/notification.component";
+import { NotificationsMenuComponent } from "./shared/notifications/notifications-menu/notifications-menu.component";
 import { PostsReceivedComponent } from "./shared/player/profile/posts-received/posts-received.component";
 import { PostsSentComponent } from "./shared/player/profile/posts-sent/posts-sent.component";
 import { FlairMarkupsComponent } from "./shared/post/flair-markup/flair-markups.component";
 import { PostComponent } from "./shared/post/post.component";
 import { ChatLogComponent } from "./shared/replay/chat-log/chat-log.component";
 import { TeamRosterComponent } from "./shared/replay/team-roster/team-roster.component";
-import { SettingsComponent } from './pages/settings/settings.component';
-import { SeedTokenChangeComponent } from './shared/modals/seed-token-change/seed-token-change.component';
-import { PostDeleteComponent } from './shared/modals/post-delete/post-delete.component';
-import { PostModDeleteComponent } from './shared/modals/post-mod-delete/post-mod-delete.component';
-import { KarmaColorPipe } from './services/pipes/karma-color.pipe';
-import { BypassHtmlPipe } from './services/pipes/bypass-html.pipe';
-import { HtmlLoaderComponent } from './shared/components/html-loader/html-loader.component';
-import { MinMaxMetricComponent } from './shared/components/minmax-metric/min-max-metric.component';
-import { PostBorderColorPipe } from './services/pipes/post-border-color.pipe';
-import { WowsNumbersPlayerLinkPipe } from './services/pipes/wows-numbers-player-link.pipe';
-import { WowsNumbersClanLinkPipe } from './services/pipes/wows-numbers-clan-link.pipe';
-import { NotificationsButtonComponent } from './shared/layout/navbar/notifications-button/notifications-button.component';
-import { NotificationsMenuComponent } from './shared/notifications/notifications-menu/notifications-menu.component';
-import { NotificationComponent } from './shared/notifications/notification/notification.component';
 
 @NgModule({
     declarations: [
@@ -106,6 +107,12 @@ import { NotificationComponent } from './shared/notifications/notification/notif
         FormsModule,
         HttpClientModule,
         ApiModule,
+        ServiceWorkerModule.register("ngsw-worker.js", {
+            // enabled: environment.production,
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: "registerWhenStable:30000",
+        }),
     ],
     providers: [
         AuthService,

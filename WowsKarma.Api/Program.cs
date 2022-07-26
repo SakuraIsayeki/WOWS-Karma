676953f63fdbac5f991669ce71787daa49759b19
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-07-26 19:10:21 +0200

    Switch logging configuration to config file based

--- WowsKarma.Api/Program.cs
+++ WowsKarma.Api/Program.cs
@@ -16,39 +16,39 @@ namespace WowsKarma.Api
 		public static async Task Main(string[] args)
 		{
 			Conversions.ConfigureMapping();
-			
+
 			using IHost host = CreateHostBuilder(args).Build();
 			using IServiceScope scope = host.Services.CreateScope();
-			
+
 			IConfiguration configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
 
-			Log.Logger = new LoggerConfiguration()
-#if DEBUG
-				.MinimumLevel.Debug()
-#else
-				.MinimumLevel.Information()
-				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-				.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
-#endif
-				.Enrich.FromLogContext()
-				.Enrich.WithProperty("_Source", typeof(Program).Assembly.GetName())
-				.Enrich.WithProperty("_Environment", configuration["environment"])
-				.Enrich.WithProperty("_Region", Startup.ApiRegion.ToRegionString())
-				.WriteTo.Console()
-#if DEBUG
-				.WriteTo.Seq(configuration["Seq:ListenUrl"], apiKey: configuration["Seq:ApiKey"])
-#endif
-				.CreateLogger();
+//			Log.Logger = new LoggerConfiguration()
+//#if DEBUG
+//				.MinimumLevel.Debug()
+//#else
+//				.MinimumLevel.Information()
+//				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+//				.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+//#endif
+//				.Enrich.FromLogContext()
+//				.Enrich.WithProperty("_Source", typeof(Program).Assembly.GetName())
+//				.Enrich.WithProperty("_Environment", configuration["environment"])
+//				.Enrich.WithProperty("_Region", Startup.ApiRegion.ToRegionString())
+//				.WriteTo.Console()
+//#if DEBUG
+//				.WriteTo.Seq(configuration["Seq:ListenUrl"], apiKey: configuration["Seq:ApiKey"])
+//#endif
+//				.CreateLogger();
 
 			Log.Information("Region selected : {Region}", Startup.ApiRegion);
-			
-			
-			using (ApiDbContext db = scope.ServiceProvider.GetRequiredService<ApiDbContext>())
+
+
+			await using (ApiDbContext db = scope.ServiceProvider.GetRequiredService<ApiDbContext>())
 			{
 				await db.Database.MigrateAsync();
 			}
 
-			using (AuthDbContext db = scope.ServiceProvider.GetRequiredService<AuthDbContext>())
+			await using (AuthDbContext db = scope.ServiceProvider.GetRequiredService<AuthDbContext>())
 			{
 				await db.Database.MigrateAsync();
 			}
@@ -62,17 +62,21 @@ namespace WowsKarma.Api
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
-					{
-						config.SetBasePath(Directory.GetCurrentDirectory());
-						config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-							  .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-							  .AddUserSecrets<Program>(optional: true, reloadOnChange: true);
-						config.AddEnvironmentVariables();
-						config.AddCommandLine(args);
-					});
+						{
+							config.SetBasePath(Directory.GetCurrentDirectory());
+							config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+								.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+								.AddUserSecrets<Program>(optional: true, reloadOnChange: true);
+							config.AddEnvironmentVariables();
+							config.AddCommandLine(args);
+						}
+					);
 
 					webBuilder.UseStartup<Startup>();
-					webBuilder.UseSerilog();
-				});
+				})
+				.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+					.ReadFrom.Configuration(hostingContext.Configuration)
+					.Enrich.WithProperty("_Region", Startup.ApiRegion.ToRegionString())
+				);
 	}
 }

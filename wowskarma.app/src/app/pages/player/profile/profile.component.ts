fca5d28fc5dea2cb3f8d4593e18c97b00095f279
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-09-30 01:52:46 +0200

    Implement CM-view profile platform ban indicator

--- wowskarma.app/src/app/pages/player/profile/profile.component.ts
+++ wowskarma.app/src/app/pages/player/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { BehaviorSubject, merge, Subject } from "rxjs";
-import { map } from "rxjs/operators";
+import { BehaviorSubject, combineLatest, merge, of, Subject, tap } from "rxjs";
+import { filter, map } from "rxjs/operators";
+import { PlatformBansService } from 'src/app/services/api/services/platform-bans.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { PlayerService } from "../../../services/api/services/player.service";
 import { getWowsNumbersPlayerLink } from "../../../services/helpers";
-import { mapApiModelState, routeParam, shareReplayRefCount, switchMapCatchError, tapAny } from "../../../shared/rxjs-operators";
+import { filterNotNull, mapApiModelState, routeParam, shareReplayRefCount, switchMapCatchError, tapAny } from "../../../shared/rxjs-operators";
 
 
 @Component({
@@ -23,6 +25,25 @@ export class ProfileComponent {
 
   profile$ = this.request$.pipe(map(result => result.model));
 
+  get isCurrentUserCM() {
+    return this.authService.isInRole('mod');
+  }
+
+  platformBans$ = this.profile$.pipe(
+    tap(() => console.debug("Fetching platform bans...")),
+    // Only send a request if the user is a CM and the profile is loaded.
+    filter(() => this.isCurrentUserCM),
+    tap(() => console.debug("User is a CM, fetching platform bans...")),
+    switchMapCatchError((profile) => this.platformBansService.apiModBansUserIdGet$Json({userId: profile!.id!})),
+    tapAny(() => console.debug("Fetched platform bans.")),
+    shareReplayRefCount(1)
+  )
+
+  isPlatformBanned$ = this.platformBans$.pipe(
+    map(bans => (bans && bans.length > 0) ?? false),
+    shareReplayRefCount(1)
+  );
+
   // Gets the profile's total karma, as a sum of game + platform karma.
   // Observable profile$ must first successfully emit a profile before calculating the total karma.
   profileTotalKarma$ = this.profile$.pipe(map(profile => (profile?.gameKarma ?? 0) + (profile?.siteKarma ?? 0)));
@@ -34,7 +55,7 @@ export class ProfileComponent {
       map(profile => profile?.optedOut ? "sent" : "received")),
   ).pipe(shareReplayRefCount(1));
 
-  constructor(private route: ActivatedRoute, private playerService: PlayerService) {
+  constructor(private route: ActivatedRoute, private playerService: PlayerService, public authService: AuthService, private platformBansService: PlatformBansService) {
   }
 
   getWowsNumbersPlayerLink(id: number, username: string): string | undefined {

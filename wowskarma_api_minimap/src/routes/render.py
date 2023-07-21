8b0bef4b5c9fa8d5832c86f083ac392315670cef
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-07-21 14:14:09 +0200

    feat(render): Add fps and quality options to replay rendering
    - Added `fps` option to control the frames per second for replay processing.
    - Added `quality` option to specify the quality of the output video.
    - Updated temporary work directory path for replay processing.

--- wowskarma_api_minimap/src/routes/render.py
+++ wowskarma_api_minimap/src/routes/render.py
@@ -5,9 +5,9 @@ import tempfile
 import os
 
 import pathlib
-from asyncio.log import logger
 
 from fastapi import APIRouter, HTTPException, status, UploadFile, BackgroundTasks
+from fastapi.logger import logger
 from fastapi.responses import FileResponse
 from renderer.data import ReplayData
 from renderer.render import Renderer
@@ -18,11 +18,13 @@ from ..security import AuthenticatedUser
 
 router = APIRouter()
 
-
+def on_job_finished(job_id: str, filepath):
+    logger.info(f"Render job {job_id} finished. Filesize: {os.stat(filepath).st_size / 1024 / 1024:.2f} MiB")
+    os.remove(filepath)
 
 
 @router.post("/render", dependencies=[AuthenticatedUser])
-async def render_replay(
+def render_replay(
         file: UploadFile,
         background_tasks: BackgroundTasks,
         replay_id: str | None = None,
@@ -43,7 +45,7 @@ async def render_replay(
         )
 
     # Log the new job.
-    job_id = binascii.b2a_hex(os.urandom(7))
+    job_id = str(binascii.b2a_hex(os.urandom(7))).split("'")[1]
     logger.info(f"Started new render job: Job ID: {job_id}, Replay ID: {replay_id or 'None'}, "
                 f"Filename: {file.filename}.")
 
@@ -51,20 +53,29 @@ async def render_replay(
     os.makedirs(settings.replay.temp_workdir, exist_ok=True)
 
     # Render time!
+    filepath = pathlib.Path(settings.replay.temp_workdir, f"{job_id}.mp4")
     replay_info = ReplayParser(file.file, True).get_info()
     replay_data: ReplayData = replay_info["hidden"]["replay_data"]
-
-    # Concat filepaths to get the full path to the replay file.
-
-
-    filepath = pathlib.Path(settings.replay.temp_workdir, f"{job_id}.mp4")
     renderer = Renderer(
-        replay_data,
+        replay_data=replay_data,
         enable_chat=True,
         team_tracers=True,
-        target_player_id=target_player_id
+        target_player_id=target_player_id,
     )
-    renderer.start(str(filepath), quality=9, fps=30)
 
-    background_tasks.add_task(os.remove, filepath)
-    return FileResponse(filepath, media_type="video/mp4", filename=f"{replay_id or 'replay'}.mp4")
+    renderer.start(
+        path=str(filepath),
+        quality=settings.replay.quality,
+        fps=settings.replay.fps
+    )
+    background_tasks.add_task(on_job_finished, job_id, filepath)
+
+    # Log the finished job.
+    logger.info(f"Render job {job_id} finished. Filesize: {os.stat(filepath).st_size / 1024 / 1024:.2f} MiB")
+
+    return FileResponse(
+        path=filepath,
+        media_type="video/mp4",
+        filename=f"{replay_id or 'replay'}.mp4"
+    )
+

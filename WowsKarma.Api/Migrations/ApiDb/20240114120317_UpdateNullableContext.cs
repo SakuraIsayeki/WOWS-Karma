1ece247d643512c00b250d8af337fae5f357f04c
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-01-14 13:05:37 +0100

    feat(migrations): Add Nullable context migrations
    This commit adds nullable context migrations to the codebase. The `UpdateNullableContext` migration updates several columns in different tables to be non-nullable. Specifically, it modifies the `Players`, `ChatMessages`, and `BlobName` columns in the `Replays` table, the `Reason` column in the `PostModActions` table, and the `Username`, `OptOutChanged`, `Tag`, `Name`, and `Description` columns in the `Clans` table. These changes ensure that these columns cannot have null values.

---
+++ WowsKarma.Api/Migrations/ApiDb/20240114120317_UpdateNullableContext.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Nodsoft.WowsReplaysUnpack.Core.Models;
+using WowsKarma.Api.Data.Models.Replays;
+
+#nullable disable
+
+namespace WowsKarma.Api.Migrations.ApiDb
+{
+    /// <inheritdoc />
+    public partial class UpdateNullableContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<IEnumerable<ReplayPlayer>>(
+                name: "Players",
+                table: "Replays",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(IEnumerable<ReplayPlayer>),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<IEnumerable<ReplayChatMessage>>(
+                name: "ChatMessages",
+                table: "Replays",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(IEnumerable<ReplayChatMessage>),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BlobName",
+                table: "Replays",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<ArenaInfo>(
+                name: "ArenaInfo",
+                table: "Replays",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(ArenaInfo),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "PostModActions",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Players",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "OptOutChanged",
+                table: "Players",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "Clans",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clans",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Clans",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<IEnumerable<ReplayPlayer>>(
+                name: "Players",
+                table: "Replays",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(IEnumerable<ReplayPlayer>),
+                oldType: "jsonb");
+
+            migrationBuilder.AlterColumn<IEnumerable<ReplayChatMessage>>(
+                name: "ChatMessages",
+                table: "Replays",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(IEnumerable<ReplayChatMessage>),
+                oldType: "jsonb");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BlobName",
+                table: "Replays",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<ArenaInfo>(
+                name: "ArenaInfo",
+                table: "Replays",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(ArenaInfo),
+                oldType: "jsonb");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "PostModActions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Players",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "OptOutChanged",
+                table: "Players",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "Clans",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clans",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Clans",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}

e47c7e7e64a9b90af1ec47bbf736a41046d18704
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2025-02-19 19:10:59 +0100

    feat(tools): add SSL certificate generation script
    - Introduced a new PowerShell script for SSL certs
    - Automates installation of mkcert via winget
    - Sets up a local Certificate Authority
    - Generates an SSL certificate for localhost and saves it in the specified folder
    
    feat: add SSL directory creation in script
    
    Created a new directory for storing SSL certificates. Updated the script to ensure the directory is created before generating the SSL certificate for localhost.
    
    build: add SSL certificate generation script to package.json
    
    Includes a new script entry for generating SSL dev certificates, facilitating easier setup for secure local development.
    
    feat: add SSL certificate options to dev server
    
    Updates the development server script to include SSL certificate and key options, enabling HTTPS support during local development for improved security and testing.

---
+++ wowskarma.app/tools/ssl-gen.ps1
@@ -0,0 +1,13 @@
+﻿# Install mkcert using winget
+winget install -e --id "FiloSottile.mkcert"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+
+# Create a local CA (Certificate Authority)
+mkcert -install
+
+#mkdir
+New-Item -ItemType Directory -Force -Path ./.ssl
+
+# Generate an SSL certificate for localhost, save in ./ssl/ folder
+mkcert -ecdsa -key-file ./.ssl/localhost.key -cert-file ./.ssl/localhost.crt localhost

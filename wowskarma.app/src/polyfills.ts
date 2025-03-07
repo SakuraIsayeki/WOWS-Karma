57a73b1b8962c3f73a7087e0ce00ba2aa570c08f
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2025-03-07 11:14:40 +0100

    feat: remove zone.js
    Eliminated zone.js and related imports to streamline the app.
    - Updated package.json to remove zone.js dependency.
    - Cleaned up environment files by removing debugging comments and imports for zone-related error handling.
    - Adjusted polyfills to exclude zone.js, enhancing performance.

--- wowskarma.app/src/polyfills.ts
+++ wowskarma.app/src/polyfills.ts
@@ -46,11 +46,6 @@ import '@angular/localize/init';
  *
  */
 
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js';  // Included with Angular CLI.
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
